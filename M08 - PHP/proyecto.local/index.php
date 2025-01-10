307eabf14bad9eb3669ce2600a8f32ef378b6b37
ForRealy <alejandrotomevalle@.com>
2025-01-10 16:01:03 +0100

    proyect twig done

---
+++ M08 - PHP/proyecto.local/index.php
@@ -0,0 +1,6 @@
+<?PHP
+require_once 'vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.html', ['name' => 'Alejandro']);
